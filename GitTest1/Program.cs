b23df71a72ce18b692fa9ea46b46d2fcc74152f2
Todd C. Page <page_todd@hotmail.com>
2014-12-17 21:16:07 -0600

    Cut 1

---
+++ GitTest1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // cut 1
+            Console.WriteLine("Cut1");
+            Console.ReadLine();
+        }
+    }
+}
